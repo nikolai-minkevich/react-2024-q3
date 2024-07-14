86bc0f6fd6af841791e96dfc540ff53dd1a91ace
Nikolai Minkevich <minkevich.nikolai@gmail.com>
2024-07-14 14:46:34 +0200

    feat: implemnet clickable cards

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,6 +1,7 @@
 import "./index.css";
 import IEpisode from "../../interfaces/IEpisode";
 import { FC, ReactElement } from "react";
+import { Link } from "react-router-dom";
 
 type TItemProps = {
   content: IEpisode;
@@ -10,25 +11,29 @@ type TItemProps = {
 const Item: FC<TItemProps> = ({ content, detailed }): ReactElement => {
   return (
     <div className="item">
-      <h4>{content.title}</h4>
-      <p>{content.usAirDate}</p>
-      {detailed && (
-        <>
-          <h3>Additional info:</h3>
-          <ul>
-            {content.titleGerman && <li>🇩🇪 {content.titleGerman}</li>}
-            {content.titleJapanese && <li>🇯🇵 {content.titleJapanese}</li>}
-            {content.titleItalian && <li>🇮🇹 {content.titleItalian}</li>}
-            {content.yearFrom && content.yearTo && (
-              <li>
-                Years in Star Trek universe: {content.yearFrom} -{" "}
-                {content.yearTo}
-              </li>
-            )}
-          </ul>
-        </>
-      )}
-    </div>
+      <Link to={`/item/${content.uid}`}>
+        <h4>{content.title}</h4>
+        <p>{content.usAirDate}</p>
+      </Link>
+      {
+        detailed && (
+          <>
+            <h3>Additional info:</h3>
+            <ul>
+              {content.titleGerman && <li>🇩🇪 {content.titleGerman}</li>}
+              {content.titleJapanese && <li>🇯🇵 {content.titleJapanese}</li>}
+              {content.titleItalian && <li>🇮🇹 {content.titleItalian}</li>}
+              {content.yearFrom && content.yearTo && (
+                <li>
+                  Years in Star Trek universe: {content.yearFrom} -{" "}
+                  {content.yearTo}
+                </li>
+              )}
+            </ul>
+          </>
+        )
+      }
+    </div >
   );
 };
 
