ebcceac82891a54e0176583f66b83f0c467d3d36
Nikolai Minkevich <minkevich.nikolai@gmail.com>
2024-07-14 11:30:32 +0200

    feat: implement simple routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
 import ReactDOM from "react-dom/client";
 import MainPage from "./components/MainPage";
 import ErrorBoundary from "./components/ErrorBoundary";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from "./components/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/test",
+    element: <h2>Test</h2>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ErrorBoundary>
-    <MainPage />
+    <RouterProvider router={router} />
   </ErrorBoundary>,
 );
